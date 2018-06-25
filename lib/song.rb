542b664cf8ad2034f52d448fba28809337d5d453
cassidytrinh <cassidytrinh@gmail.com>
2018-06-25 13:45:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,49 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists.push(@artist)
+    @@genres.push(@genre)
+  end
+
+  def name
+    @name
+  end
+
+  def artists
+    @artist
+  end
+
+  def genre
+    @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  def genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash.key?(genre) == false
+        genre_hash[genre] = 0
+      end
+      genre_hash[genre] += 1
+    end
+  end
